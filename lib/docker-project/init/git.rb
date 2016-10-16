0524f7d3a1f5d9b6fa4fab091205350e37be07eb
Robert Northard <robertnorthard@googlemail.com>
2016-10-16 01:09:50 +0100

    Git clone docker project structure

--- lib/docker-project/init/git.rb
+++ lib/docker-project/init/git.rb
@@ -4,12 +4,11 @@ module DockerProject
   module Init
 
   	PROJECT_REPOSITORY='.'
+  	DOCKER_PROJECT_SPECIFICATION='https://github.com/RobertNorthard/docker-project-structure.git'
 
   	class GitRepo
-  	  def initialise()
-
-  	  	Git.init(Init::PROJECT_REPOSITORY)
-
+  	  def initialise(clone_directory='')
+  	  	Git.clone(Init::DOCKER_PROJECT_SPECIFICATION, clone_directory == '' ? Init::PROJECT_REPOSITORY : clone_directory )
   	  end
   	end
   end
--- test/integration/cucumber/features/command_line.feature
+++ test/integration/cucumber/features/command_line.feature
@@ -1,5 +1,6 @@
 Feature: Command Line
 
-	Scenario: Version
-		When I run the "docker-project" program
-		Then I should see that I am using docker-project version "0.0.1"
+  Scenario: Version
+	When I run the "docker-project" program
+    Then I should see that I am using docker-project version "0.0.1"
+
